d9e262420bac78b572419b9b026a7ccf4fc8f3d9
Amirreza <arazarioun83@gmail.com>
2025-07-03 20:16:12 +0330

    Fix: Config for Pictures

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "api.taksize.com",
-        pathname: "**",
       },
     ],
   },
--- src/Components/SliderStory/SliderStory.jsx
+++ src/Components/SliderStory/SliderStory.jsx
@@ -1,25 +1,22 @@
 "use client";
 
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
 import "./styles.css";
 
-// import required modules
 import { Pagination } from "swiper/modules";
 import Image from "next/image";
 import Link from "next/link";
 const SliderStory = ({ data }) => {
+  console.log(data);
+
   return (
     <div className="w-full max-w-4xl mx-auto">
       <Swiper
-        // slidesPerView={8}
-        // spaceBetween={30}
         pagination={{
           clickable: true,
         }}
